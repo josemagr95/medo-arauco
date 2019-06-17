dcecd211667272481b492b650d6db22507b07b3f
benitogonzalezh <benitogonzalezh@gmail.com>
2019-06-17 16:14:36 -0400

    Hello World

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const http = require('http');
-let fs = require('fs');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
 });
 
 const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
